a1fc4821eba611837f9fd0ffb7eea4fd7c5f4d8b
Mariklimova <mariku22@yandex.ru>
2024-11-02 12:01:17 +0300

    added animation to the button and fixed the code in Experience and About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,15 +11,25 @@ const animation = {
         opacity: 1,
         transition: { delay: custom * 0.2 },
     }),
-    hiddenVertical: {
-        y: -100,
+    hiddenButton: {
+        x: 100,
         opacity: 0,
     },
-    visibleVertical: (custom) => ({
-        y: 0,
+    visibleButton: (custom) => ({
+        x: 0,
         opacity: 1,
-        transition: { delay: custom * 0.2 },
+        transition: { delay: custom * 0.3 },
+        boxShadow: "5px 5px 5px rgba(5, 5, 5, 0.5)",
     }),
+    whileHover: {
+        scale: 1.1,
+        boxShadow: "8px 8px 8px rgba(8, 8, 8, 0.8)",
+        cursor: "pointer",
+    },
+    whileTap: {
+        boxShadow: "0px 0px 0px rgba(0, 0, 0, 0)",
+        scale: 1,
+    },
 };
 
 export default function About() {
@@ -39,9 +49,16 @@ export default function About() {
                 <motion.h2 custom={1} variants={animation}>Свяжитесь со мной</motion.h2>
                 <motion.p custom={2} variants={animation}>если, Вам нужен увлечённый Fullstack разработчик, который стремиться создавать не только функциональные, но красивые и удобные интерфейсы, привлекающие пользователей.
                     Я изучила множество технологий, таких как HTML, CSS/SCSS, JavaScript и TypeScript, а также серверные языки, такие как Node.js. Я также работала с различными фреймворками, такими как React и Express, что позволяет мне быть гибкой и адаптироваться к требованиям проекта.
-                    В моём портфолио Вы найдёте проекты, в которых я использовал современные технологии и подходы. Если Вы ищете разработчика, готового внедрять инновации и работать над интересными проектами.
-                    Я всегда открыта для новых возможностей и готова к сотрудничеству. Давайте вместе создадим нечто уникальное и впечатляющее!</motion.p>
-                <motion.button custom={3} variants={animation} onClick={handleContactClick}>Мои контакты</motion.button>
+                    В моём портфолио Вы найдёте проекты, в которых я использовала современные технологии и подходы. Если Вы ищете разработчика, готового внедрять инновации и работать над интересными проектами, я всегда открыта для новых возможностей и готова к сотрудничеству. Давайте вместе создадим нечто уникальное и впечатляющее!
+                </motion.p>
+                <motion.button custom={3}
+                    initial="hiddenButton"
+                    whileInView="visibleButton"
+                    viewport={{ once: true }}
+                    whileHover={animation.whileHover}
+                    whileTap={animation.whileTap}
+                    variants={animation} onClick={handleContactClick}>Мои контакты
+                </motion.button>
 
             </div>
         </motion.div>
